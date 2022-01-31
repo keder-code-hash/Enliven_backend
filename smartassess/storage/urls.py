4f6cf5f5c1290b9b94e3fb5e9c4e1790077736c9
Purnadip Manna <purnadipmanna2702@gmail.com>
2022-02-01 01:16:18 +0530

    set_qna_updated2

--- smartassess/storage/urls.py
+++ smartassess/storage/urls.py
@@ -5,6 +5,7 @@ urlpatterns = (
     [
         path("save/", save_questions, name="tempSaveQnA"),
         path("saveexamname/", save_examname, name="saveExamName"),
-        path("fetchqna/", fetch_questions, name="fetchQnA")
+        path("fetchqna/", fetch_questions, name="fetchQnA"),
+        path("deleteqna/", delete_questions, name="deleteQnA")
     ]
 )
