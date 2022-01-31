6b4c617edaf6788a03456d7e08f95d993e9b2979
Purnadip Manna <purnadipmanna2702@gmail.com>
2022-01-31 12:05:51 +0530

    setQn_ui_updated

--- smartassess/assessment/views.py
+++ smartassess/assessment/views.py
@@ -138,12 +138,18 @@ def self_assessment(request):
 
 # set questions for examination (accessible by teacher only)
 def set_questions(request):
-    questions_list = [1, 2, 3, 4, 5, 6]
+    questions_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]
     context = {
         "questions_num": questions_list,
         "is_authenticated": is_authenticated_user(request),
     }
-    return render(request, "Examset.html", context)
+    if context["is_authenticated"]:
+        return render(request, "Examset.html", context)
+    else:
+        err_log = {
+            "msg" : "Are you logged in?"
+        }
+        return render(request, "Error.html", err_log)
 
 
 # view results of all students (accessible by teacher only)
