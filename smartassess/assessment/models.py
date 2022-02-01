95a813ca1f5469351816a4f061cdd17e50255247
arghyasahoo <arghyasahoo001@gmail.com>
2022-02-01 20:17:29 +0530

    Added max_length to Charfield

--- smartassess/assessment/models.py
+++ smartassess/assessment/models.py
@@ -4,10 +4,12 @@ from users.models import Course, Register
 
 class Exam(models.Model):
     _id = models.BigIntegerField(unique=True, primary_key=True, blank=False)
-    exam_name = models.CharField(blank=False)
+    exam_name = models.CharField(max_length=130, blank=False)
     marks = models.IntegerField(blank=False)
-    course = models.CharField(blank=False)
-    description = models.CharField()
+    course = models.CharField(max_length=130, blank=False)
+    description = models.CharField(
+        max_length=130,
+    )
     duration = models.TimeField(blank=False)
     created_by = models.ManyToManyField(Register)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -16,8 +18,8 @@ class Exam(models.Model):
 class Question(models.Model):
     _id = models.BigIntegerField(unique=True, primary_key=True, blank=False)
     exam_id = models.BigIntegerField(blank=False)
-    question = models.CharField(blank=False)
-    standard_ans = models.CharField(blank=False)
+    question = models.CharField(max_length=130, blank=False)
+    standard_ans = models.CharField(max_length=130, blank=False)
     created_by = models.ManyToManyField(Register)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -26,7 +28,7 @@ class Answer(models.Model):
     _id = models.BigIntegerField(unique=True, primary_key=True, blank=False)
     exam_id = models.BigIntegerField(blank=False)
     question_id = models.BigIntegerField(unique=True, blank=False)
-    answer = models.CharField(blank=False)
+    answer = models.CharField(max_length=130, blank=False)
     answer_duration = models.TimeField(blank=False)
     answered_by = models.ManyToManyField(Register)
     answered_at = models.DateTimeField(auto_now_add=True)
@@ -37,7 +39,9 @@ class Evaluation(models.Model):
     exam_id = models.BigIntegerField(blank=False)
     question_id = models.BigIntegerField(unique=True, blank=False)
     answer_id = models.BigIntegerField(unique=True, blank=False)
-    remarks = models.CharField()
+    remarks = models.CharField(
+        max_length=130,
+    )
     score = models.IntegerField(blank=False)
     match_percentage = models.FloatField(blank=False)
 
