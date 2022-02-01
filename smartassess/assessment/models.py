97935755f657b6b5fcadb8ffea08946ae0758ef9
keder-code-hash <kedernath.mallick.tint022@gmail.com>
2022-02-01 13:46:01 +0530

    assessment db updated

--- smartassess/assessment/models.py
+++ smartassess/assessment/models.py
@@ -2,58 +2,42 @@ from django.db import models
 from users.models import Course, Register
 
 
-class Question(models.Model):
-    # question_id = models.BigIntegerField(unique=True, primary_key=True,auto_created=True)
-    question = models.CharField(max_length=100, unique=True)
-    marks = models.IntegerField(blank=False)
+'''
+Exam
+    id
+    exam_name
+    marks
+    course
+    desc
+    time
+    created_by
+    created_at
+    
+Question
+    id
+    exam_id
+    question
+    standard_ans 
+    question_created_by 
+    created_at
 
-    DIFFICULTY = (("E", "Easy"), ("M", "Medium"), ("H", "Hard"))
-    level = models.CharField(max_length=20, choices=DIFFICULTY, default="E")
+Answer
+    id 
+    exam_id
+    question_id
+    ansewer
+    answered_duration
+    answered_by
+    answered_at
 
-
-class Answer(models.Model):
-    # ans_id = models.BigIntegerField(unique=True, primary_key=True,auto_created=True)
-    answer = models.CharField(max_length=130)
-    std_id = models.ManyToManyField(Register, related_name="answerd_by")
-    # std_id = models.OneToOneField(Register,on_delete=models.CASCADE)
-
-
-
-# set questions
-class StandardQnA(models.Model):
-    # _id = models.BigIntegerField(primary_key=True, unique=True,auto_created=True)
-    question = models.ManyToManyField(Question)
-    std_ans = models.ManyToManyField(Answer) 
-
-
-class QnA(models.Model):
-    question = models.ManyToManyField(Question )
-    student_ans = models.ManyToManyField(Answer )
-
-
-# take exam
-class Exam(models.Model):
-    # _id = models.BigIntegerField(primary_key=True, unique=True,auto_created=True)
-    exam_name=models.CharField(max_length=50,blank=False)
-    course = models.ManyToManyField(Course, related_name="course")
-    standard_qna = models.ManyToManyField(StandardQnA)
-    date = models.DateTimeField(auto_now_add=True)
-    duration = models.TimeField(blank=False)
-    total_questions = models.IntegerField()
-    full_marks = models.IntegerField()
-    detailed_information=models.CharField(max_length=200,default='')
+Evaluation
+    id
+    exam_id
+    question_id 
+    answer_id
+    remarks
+    marks
+    matched_percentage
 
 
-class Result(models.Model):
-    # _id = models.BigIntegerField(primary_key=True, unique=True,auto_created=True)
-    standard_qna = models.OneToOneField(StandardQnA, on_delete=models.CASCADE)
-    answer = models.OneToOneField(Answer, on_delete=models.CASCADE)
-    marks_obtained = models.FloatField(blank=False)
-
-
-class AssessmentResult(models.Model):
-    # _id = models.BigIntegerField(primary_key=True, unique=True,auto_created=True)
-    exam_id = models.OneToOneField(Exam, on_delete=models.CASCADE)
-    result = models.ManyToManyField(Result)
-    total_marks_obtained = models.IntegerField(blank=False)
-    evaluation_date = models.DateTimeField(auto_now_add=True)
+'''
--- smartassess/assessment/urls.py
+++ smartassess/assessment/urls.py
@@ -3,17 +3,13 @@ from django.urls.conf import include
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import set_questions, test_results, view_all_results
-
-from assessment.queries import *
+ 
 
 urlpatterns = (
     [
         path("setquestion/", set_questions, name="setquestion"),
         path("view_all_results/", view_all_results, name="view_all_results"),
-        path("result/", test_results, name="result"),
-
-
-        path('test/',test,name="modeltest")
+        path("result/", test_results, name="result")
     ]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
