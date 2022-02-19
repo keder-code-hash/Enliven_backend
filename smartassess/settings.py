edd5a9ec8fbab5072df3d74e4d91d3d3281afc3b
Purnadip Manna <purnadipmanna2702@gmail.com>
2022-02-19 23:22:43 +0530

    deployment_5

--- smartassess/settings.py
+++ smartassess/settings.py
@@ -125,7 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [
     "static",
